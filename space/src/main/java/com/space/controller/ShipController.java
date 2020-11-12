75d5b5fd633180a37b0935ff28b1588b0825fd8f
KorsakovAA <Korsakowaa@mail,ru>
2020-11-12 17:14:36 +0300

    Add getShipCount

--- space/src/main/java/com/space/controller/ShipController.java
+++ space/src/main/java/com/space/controller/ShipController.java
@@ -58,6 +58,178 @@ public class ShipController {
                     continue;
                 }
             }
+            if (planet != null) {
+                Pattern pattern = Pattern.compile(".*" + planet + ".*");
+                if (!(matcher = pattern.matcher(ships.get(i).getPlanet())).matches()) {
+                    ships.remove(i);
+                    i--;
+                    continue;
+                }
+            }
+            if (shipType != null) {
+                ShipType type = ShipType.valueOf(shipType);
+                if (!(type.equals(ships.get(i).getShipType()))) {
+                    ships.remove(i);
+                    i--;
+                    continue;
+                }
+            }
+            if (after != null) {
+                try {
+                    Date previousDate = new Date(Long.parseLong(after));
+                    if (ships.get(i).getProdDate().before(previousDate)) {
+                        ships.remove(i);
+                        i--;
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Параметр \"after\" не числовой");
+                }
+            }
+            if (before != null) {
+                try {
+                    Date pastDate = new Date(Long.parseLong(before));
+                    if (ships.get(i).getProdDate().after(pastDate)) {
+                        ships.remove(i);
+                        i--;
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Параметр \"before\" не числовой");
+                }
+            }
+            if (isUsed != null) {
+                try {
+                    boolean used = Boolean.parseBoolean(isUsed);
+                    if (ships.get(i).getUsed() != used) {
+                        ships.remove(i);
+                        i--;
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Параметр \"isUsed\" не boolean");
+                }
+            }
+            if (minSpeed != null) {
+                try {
+                    Double doubleMinSpeed = Double.parseDouble(minSpeed);
+                    if (ships.get(i).getSpeed() < doubleMinSpeed) {
+                        ships.remove(i);
+                        i--;
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Параметр \"minSpeed\" не double");
+                }
+            }
+            if (maxSpeed != null) {
+                try {
+                    Double doubleMaxSpeed = Double.parseDouble(maxSpeed);
+                    if (ships.get(i).getSpeed() > doubleMaxSpeed) {
+                        ships.remove(i);
+                        i--;
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Параметр \"maxSpeed\" не double");
+                }
+            }
+            if (minCrewSize != null) {
+                try {
+                    Integer integerMinCrewSize = Integer.parseInt(minCrewSize);
+                    if (ships.get(i).getCrewSize() < integerMinCrewSize) {
+                        ships.remove(i);
+                        i--;
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Параметр \"minCrewSize\" не int");
+                }
+            }
+            if (maxCrewSize != null) {
+                try {
+                    Integer integerMaxCrewSize = Integer.parseInt(maxCrewSize);
+                    if (ships.get(i).getCrewSize() > integerMaxCrewSize) {
+                        ships.remove(i);
+                        i--;
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Параметр \"maxCrewSize\" не int");
+                }
+            }
+            if (minRating != null) {
+                try {
+                    Double doubleMinRating = Double.parseDouble(minRating);
+                    if (ships.get(i).getRating() < doubleMinRating) {
+                        ships.remove(i);
+                        i--;
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Параметр \"minRating\" не double");
+                }
+            }
+            if (maxRating != null) {
+                try {
+                    Double doubleMaxRating = Double.parseDouble(maxRating);
+                    if (ships.get(i).getRating() > doubleMaxRating) {
+                        ships.remove(i);
+                        i--;
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Параметр \"maxRating\" не double");
+                }
+            }
+        }
+            // Order, PageNumber, PageSize
+            if(order != null){
+                ShipOrder shipOrder = ShipOrder.valueOf(order);
+                if(shipOrder.equals(ShipOrder.ID)){
+                    Collections.sort(ships, comparatorById);
+                } else if(shipOrder.equals(ShipOrder.DATE)){
+                    Collections.sort(ships, comparatorByDate);
+                } else if(shipOrder.equals(ShipOrder.SPEED)){
+                    Collections.sort(ships, comparatorBySpeed);
+                } else if(shipOrder.equals(ShipOrder.RATING)){
+                    Collections.sort(ships, comparatorByRating);
+                }
+            }
+            if(pageNumber == null) pageNumber = "0";
+            if(pageSize == null) pageSize = "3";
+            ships = getPages(ships, pageNumber, pageSize);
+
+        return new ResponseEntity<>(ships, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "ships/count", method = RequestMethod.GET)
+    public ResponseEntity<Integer> getShipsCount(@RequestParam(value = "name", required = false) String name,
+                                                  @RequestParam(value = "planet", required = false) String planet,
+                                                  @RequestParam(value = "shipType", required = false) String shipType,
+                                                  @RequestParam(value = "after", required = false) String after,
+                                                  @RequestParam(value = "before", required = false) String before,
+                                                  @RequestParam(value = "isUsed", required = false) String isUsed,
+                                                  @RequestParam(value = "minSpeed", required = false) String minSpeed,
+                                                  @RequestParam(value = "maxSpeed", required = false) String maxSpeed,
+                                                  @RequestParam(value = "minCrewSize", required = false) String minCrewSize,
+                                                  @RequestParam(value = "maxCrewSize", required = false) String maxCrewSize,
+                                                  @RequestParam(value = "minRating", required = false) String minRating,
+                                                  @RequestParam(value = "maxRating", required = false) String maxRating) {
+
+        List<Ship> ships = this.shipService.getAll();
+        if (ships.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        for (int i = 0; i < ships.size(); i++) {
+            if (name != null) {
+                Pattern pattern = Pattern.compile(".*" + name + ".*");
+                if (!(matcher = pattern.matcher(ships.get(i).getName())).matches()) {
+                    ships.remove(i);
+                    i--;
+                    continue;
+                }
+            }
             if(planet != null){
                 Pattern pattern = Pattern.compile(".*" + planet + ".*");
                 if (!(matcher = pattern.matcher(ships.get(i).getPlanet())).matches()) {
@@ -168,22 +340,8 @@ public class ShipController {
                     }
                 }catch (NumberFormatException e){System.out.println("Параметр \"maxRating\" не double");}
             }
-            // Order, PageNumber, PageSize
-            if(order != null){
-                ShipOrder shipOrder = ShipOrder.valueOf(order);
-                if(shipOrder.equals(ShipOrder.ID)){
-                    Collections.sort(ships, comparatorById);
-                } else if(shipOrder.equals(ShipOrder.DATE)){
-                    Collections.sort(ships, comparatorByDate);
-                } else if(shipOrder.equals(ShipOrder.SPEED)){
-                    Collections.sort(ships, comparatorBySpeed);
-                } else if(shipOrder.equals(ShipOrder.RATING)){
-                    Collections.sort(ships, comparatorByRating);
-                }
-            }
-
         }
-        return new ResponseEntity<>(ships, HttpStatus.OK);
+        return new ResponseEntity<>(ships.size(), HttpStatus.OK);
     }
 
 
@@ -241,8 +399,16 @@ public class ShipController {
         if(currentShip == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        if(enterShip.getName() != null && enterShip.getName().length() <= 50) currentShip.setName(enterShip.getName());
-        if(enterShip.getPlanet() != null && enterShip.getPlanet().length() <= 50) currentShip.setPlanet(enterShip.getPlanet());
+        if(enterShip.getName() != null){
+            if(enterShip.getName().length() <= 50 && enterShip.getName().length()>0) currentShip.setName(enterShip.getName());
+            else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if(enterShip.getPlanet() != null){
+            if(enterShip.getPlanet().length() <= 50) currentShip.setPlanet(enterShip.getPlanet());
+            else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         if(enterShip.getShipType() != null) currentShip.setShipType(enterShip.getShipType());
         if(enterShip.getProdDate() != null){
             Calendar calendar = new GregorianCalendar();
@@ -250,16 +416,24 @@ public class ShipController {
             Calendar calendar2 = new GregorianCalendar();
             calendar2.set(3019, 11,30);
             if(enterShip.getProdDate().getTime() >= calendar.getTimeInMillis() && enterShip.getProdDate().getTime() <= calendar2.getTimeInMillis()) currentShip.setProdDate(enterShip.getProdDate());
+            else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         if(enterShip.getUsed() != null) currentShip.setUsed(enterShip.getUsed());
-        if(enterShip.getSpeed() != null && enterShip.getSpeed() >= 0.01 && enterShip.getSpeed() <= 0.99){
-            DecimalFormat decimalFormat = new DecimalFormat( "#.##" );
-            String speed = decimalFormat.format(enterShip.getSpeed());
-            speed = speed.replace(",", ".");
-            Double dSpeed = Double.parseDouble(speed);
-            currentShip.setSpeed(dSpeed);
+
+        if(enterShip.getSpeed() != null) {
+            if(enterShip.getSpeed() >= 0.01 && enterShip.getSpeed() <= 0.99) {
+                DecimalFormat decimalFormat = new DecimalFormat("#.##");
+                String speed = decimalFormat.format(enterShip.getSpeed());
+                speed = speed.replace(",", ".");
+                Double dSpeed = Double.parseDouble(speed);
+                currentShip.setSpeed(dSpeed);
+            } else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        if(enterShip.getCrewSize() != null && enterShip.getCrewSize() >= 1 && enterShip.getCrewSize() <= 9999) currentShip.setCrewSize(enterShip.getCrewSize());
+        if(enterShip.getCrewSize() != null) {
+            if(enterShip.getCrewSize() >= 1 && enterShip.getCrewSize() <= 9999) currentShip.setCrewSize(enterShip.getCrewSize());
+            else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         currentShip.setRating(calculateRating(currentShip));
         shipService.update(currentShip);
 
@@ -278,6 +452,21 @@ public class ShipController {
         shipService.delete(shipId);
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private List<Ship> getPages(List<Ship> fullList, String pageNumber, String pageSize){
+        try {
+            int pgNumber = Integer.parseInt(pageNumber);
+            int pgSize = Integer.parseInt(pageSize);
+            int numberOfPage = fullList.size() % pgSize == 0 ? (fullList.size() / pgSize) : (fullList.size() / pgSize) + 1;
+            if(pgNumber > numberOfPage - 1) pgNumber = 0;
+            int beginWith = pgNumber * pgSize > fullList.size()-1 ? fullList.size()-1 : pgNumber * pgSize;
+            int stopWith = beginWith + pgSize > fullList.size() ? fullList.size() : beginWith + pgSize;
+            return fullList.subList(beginWith, stopWith);
+        } catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+        return fullList;
     }
 
     private Double calculateRating(Ship ship){
